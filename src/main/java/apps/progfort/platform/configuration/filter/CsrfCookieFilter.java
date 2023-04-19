cc3f06279487abe7e09ce7cf221f48148e0fe299
Restyled.io <commits@restyled.io>
2023-04-19 00:44:43 +0000

    Restyled by clang-format

--- src/main/java/apps/progfort/platform/configuration/filter/CsrfCookieFilter.java
+++ src/main/java/apps/progfort/platform/configuration/filter/CsrfCookieFilter.java
@@ -4,23 +4,22 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
-
 public class CsrfCookieFilter extends OncePerRequestFilter {
-    @Override
-    protected void doFilterInternal(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain
-    ) throws ServletException, IOException {
+  @Override
+  protected void doFilterInternal(HttpServletRequest request,
+                                  HttpServletResponse response,
+                                  FilterChain filterChain)
+      throws ServletException, IOException {
 
-        CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
-        if (null != csrfToken.getHeaderName()) {
-            response.setHeader(csrfToken.getHeaderName(), csrfToken.getToken());
-        }
-        filterChain.doFilter(request, response);
+    CsrfToken csrfToken =
+        (CsrfToken)request.getAttribute(CsrfToken.class.getName());
+    if (null != csrfToken.getHeaderName()) {
+      response.setHeader(csrfToken.getHeaderName(), csrfToken.getToken());
     }
+    filterChain.doFilter(request, response);
+  }
 }
--- src/main/java/apps/progfort/platform/configuration/security/SecurityConfiguration.java
+++ src/main/java/apps/progfort/platform/configuration/security/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package apps.progfort.platform.configuration.security;
 
 import apps.progfort.platform.configuration.filter.CsrfCookieFilter;
+import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,46 +14,49 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 
-import java.util.Collections;
-
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        CsrfTokenRequestAttributeHandler csrfToken = new CsrfTokenRequestAttributeHandler();
-        csrfToken.setCsrfRequestAttributeName("_csrf");
-        http
-                .securityContext()
-                .requireExplicitSave(false)
-                .and()
-                .sessionManagement(
-                        session -> session
-                                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
-                )
-                .cors()
-                .configurationSource(
-                        request -> {
-                            CorsConfiguration cors = new CorsConfiguration();
-                            cors.setAllowedOrigins(Collections.singletonList("*"));
-                            cors.setAllowedMethods(Collections.singletonList("http://localhost:3000"));
-                            cors.setAllowCredentials(true);
-                            cors.setAllowedHeaders(Collections.singletonList("*"));
-                            cors.setMaxAge(3600L);
-                            return cors;
-                        }).and()
-                .csrf((csrf) -> csrf.csrfTokenRequestHandler(csrfToken)
-                        .ignoringRequestMatchers("/api/**")
-                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
-                .addFilterAt(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
-                .authorizeHttpRequests()
-                .requestMatchers("/api/**").permitAll()
-                .and().formLogin()
-                .and().httpBasic();
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http)
+      throws Exception {
+    CsrfTokenRequestAttributeHandler csrfToken =
+        new CsrfTokenRequestAttributeHandler();
+    csrfToken.setCsrfRequestAttributeName("_csrf");
+    http.securityContext()
+        .requireExplicitSave(false)
+        .and()
+        .sessionManagement(
+            session
+            -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+        .cors()
+        .configurationSource(request -> {
+          CorsConfiguration cors = new CorsConfiguration();
+          cors.setAllowedOrigins(Collections.singletonList("*"));
+          cors.setAllowedMethods(
+              Collections.singletonList("http://localhost:3000"));
+          cors.setAllowCredentials(true);
+          cors.setAllowedHeaders(Collections.singletonList("*"));
+          cors.setMaxAge(3600L);
+          return cors;
+        })
+        .and()
+        .csrf((csrf)
+                  -> csrf.csrfTokenRequestHandler(csrfToken)
+                         .ignoringRequestMatchers("/api/**")
+                         .csrfTokenRepository(
+                             CookieCsrfTokenRepository.withHttpOnlyFalse()))
+        .addFilterAt(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+        .authorizeHttpRequests()
+        .requestMatchers("/api/**")
+        .permitAll()
+        .and()
+        .formLogin()
+        .and()
+        .httpBasic();
 
-        return http.build();
-    }
+    return http.build();
+  }
 }
-
