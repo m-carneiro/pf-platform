ed8d827331803d46c4a042ba4da72c83c3051299
Restyled.io <commits@restyled.io>
2023-05-05 02:25:12 +0000

    Restyled by clang-format

--- src/main/java/apps/progfort/platform/configuration/webClient/WebClientConfiguration.java
+++ src/main/java/apps/progfort/platform/configuration/webClient/WebClientConfiguration.java
@@ -3,6 +3,8 @@ package apps.progfort.platform.configuration.webClient;
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
+import java.util.Base64;
+import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,29 +14,29 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.tcp.TcpClient;
 
-import java.util.Base64;
-import java.util.concurrent.TimeUnit;
-
 @Configuration
 public class WebClientConfiguration {
 
-    @Value("${vimeo.url}")
-    private String baseUrl;
-    private static final int MAX_MEMORY_SIZE = 262144;
-    private static final int TIMEOUT = 1000;
+  @Value("${vimeo.url}") private String baseUrl;
+  private static final int MAX_MEMORY_SIZE = 262144;
+  private static final int TIMEOUT = 1000;
 
-    @Bean
-    public static WebClient create() {
-        HttpClient httpClient = HttpClient.create()
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, TIMEOUT)
-                .doOnConnected(connection -> {
-                    connection.addHandlerLast(new ReadTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
-                    connection.addHandlerLast(new WriteTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
-                });
+  @Bean
+  public static WebClient create() {
+    HttpClient httpClient =
+        HttpClient.create()
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, TIMEOUT)
+            .doOnConnected(connection -> {
+              connection.addHandlerLast(
+                  new ReadTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
+              connection.addHandlerLast(
+                  new WriteTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS));
+            });
 
-        return WebClient.builder()
-                .clientConnector(new ReactorClientHttpConnector(httpClient))
-                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(MAX_MEMORY_SIZE))
-                .build();
-    }
+    return WebClient.builder()
+        .clientConnector(new ReactorClientHttpConnector(httpClient))
+        .codecs(configurer
+                -> configurer.defaultCodecs().maxInMemorySize(MAX_MEMORY_SIZE))
+        .build();
+  }
 }
